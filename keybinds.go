5aa99adf6112f3a541b12ec9efff84f74101878b
carter <c.melnychuk97@gmail.com>
2024-11-20 21:53:42 -0700

    feat: consolidate key handling

--- keybinds.go
+++ keybinds.go
@@ -1,10 +1,13 @@
 package main
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+)
 
 // checks if the given key slices are equal (contain same keys).
 // assumes the keys are in the same order
-func KeysEqual(a, b []ebiten.Key) bool {
+func keysEqual(a, b []ebiten.Key) bool {
 	if len(a) != len(b) {
 		return false
 	}
@@ -18,7 +21,36 @@ func KeysEqual(a, b []ebiten.Key) bool {
 	return true
 }
 
+// reads currently pressed keys, returning the specified keybind
+func HandleKeyBindRead() Keybind {
+	// TODO: use allocated slice for more efficient reads? dont rly care
+	keys := inpututil.AppendPressedKeys(nil)
+	if len(keys) == 0 {
+		return KeybindNone
+	}
+
+	for k, v := range keyBindMap {
+		if keysEqual(keys, v) {
+			return k
+		}
+	}
+
+	return KeybindNone
+}
+
+type Keybind int
+
 var (
-	KeybindUndo = []ebiten.Key{ebiten.KeyZ, ebiten.KeyControlLeft, ebiten.KeyControl}
-	KeybindDrag = []ebiten.Key{ebiten.KeySpace}
+	KeybindNone Keybind = -1
+	KeybindUndo Keybind = 0
+	KeybindRedo Keybind = 1
+	KeybindDrag Keybind = 2
 )
+
+// registry of all possible keybinds
+var keyBindMap = map[Keybind][]ebiten.Key{
+	// TODO: agnostic keys? im hard coding for left ctrl / shift because its exactly what ebiten returns
+	KeybindUndo: []ebiten.Key{ebiten.KeyZ, ebiten.KeyControlLeft, ebiten.KeyControl},
+	KeybindRedo: []ebiten.Key{ebiten.KeyZ, ebiten.KeyControlLeft, ebiten.KeyShiftLeft, ebiten.KeyControl, ebiten.KeyShift},
+	KeybindDrag: []ebiten.Key{ebiten.KeySpace},
+}
