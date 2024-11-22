d4949cffc4ae21fa5d656d06cfd252ca3687087b
carter <c.melnychuk97@gmail.com>
2024-11-21 21:49:26 -0700

    feat: add versioning

--- main/main.go
+++ main/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"cartermel/scribble"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
+const VERSION = "1.0.0"
+
 func main() {
+	for _, arg := range os.Args[1:] {
+		if arg == "-v" || arg == "--version" {
+			fmt.Println("scribble version", VERSION)
+			os.Exit(0)
+		}
+	}
+
 	ebiten.SetWindowSize(1280, 720)
 	ebiten.SetWindowTitle("scribble")
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
